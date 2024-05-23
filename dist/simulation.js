b1d26f56146e61f73d8f1990c66118f22f4b8607
Jackson Otto <jacksonotto120@gmail.com>
2024-05-22 22:11:01 -0700

    fix object instancing

--- dist/simulation.js
+++ dist/simulation.js
@@ -12,7 +12,7 @@ struct Uniforms {
  
 @group(0) @binding(0) var<uniform> uniforms : Uniforms;
 
-@group(0) @binding(1) var<storage, read> instanceMatrices : array<mat4x4f, 10>;
+@group(0) @binding(1) var<storage, read> instanceMatrices : array<mat4x4f>;
 
 struct VertexOutput {
   @builtin(position) Position : vec4<f32>,
