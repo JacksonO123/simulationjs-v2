29e742b51c327254e886835fbe226b61abdcb2c7
Jackson Otto <jacksonotto120@gmail.com>
2024-09-30 16:15:30 -0700

    update exports

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from './utils.js';
 export * from './types.js';
 export * from './shaders.js';
 export * from './materials.js';
-export { vec2, vec3, vec4, mat3, mat4 } from 'wgpu-matrix';
+export * from './constants.js';
+export { vec2, vec3, vec4, mat3, mat4, quat } from 'wgpu-matrix';
