2056a6ec7c6048998471a103a61fdf51324ff260
Jackson Otto <jacksonotto120@gmail.com>
2024-10-17 14:11:57 -0700

    remove dead comments

--- test/transform.ts
+++ test/transform.ts
@@ -26,20 +26,3 @@ const square = new Square(vector2(500, -500), 200, 200);
 setTimeout(async () => {
   transform(circle, square, 2, easeInOutQuad);
 }, 1000);
-
-// const square = new Square(vector2(500, -500), 200, 200);
-// square.setWireframe(true);
-// canvas.add(square);
-// square.setSubdivisions(5);
-// const squareVertices = square.getVertices();
-
-// const circle = new Circle(vector2(300, -300), 100, canvas.getDefaultColor(), squareVertices.length);
-// const circleVertices = cloneVectors(circle.getVertices());
-// canvas.add(circle);
-
-// const polygon = new Polygon(vector2(700, -700), vectorsToVertex(circleVertices));
-// canvas.add(polygon);
-
-// setTimeout(() => {
-//   polygon.setVertices(vectorsToVertex(squareVertices), 1, easeInOutQuart);
-// }, 500);
